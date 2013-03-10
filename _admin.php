eb87e8458c398fea711e070e17a389f43906aa1a
hiteule <hiteule@bozo-laptop.(none)>
2013-03-10 18:20:18 +0100

    Add favorite support

--- _admin.php
+++ _admin.php
@@ -5,7 +5,6 @@ if (!defined('DC_CONTEXT_ADMIN')) {
 
 require dirname(__FILE__) . '/_widgets.php';
 
-# ajouter le plugin dans la liste des plugins du menu de l'administration
 $_menu['Plugins']->addItem(
 	__('My Twitter'),
 	'plugin.php?p=myTwitter',
@@ -13,4 +12,23 @@ $_menu['Plugins']->addItem(
 	preg_match('/plugin.php\?p=myTwitter(&.*)?$/', $_SERVER['REQUEST_URI']),
 	$core->auth->check('usage,contentadmin', $core->blog->id)
 );
+
+$core->addBehavior('adminDashboardFavs', array('myTwitterBehaviors', 'dashboardFavs'));
+
+class myTwitterBehaviors
+{
+	public static function dashboardFavs($core, $favs)
+	{
+		$favs['myTwitter'] = new ArrayObject(array(
+			'myTwitter',
+			__('My Twitter'),
+			'plugin.php?p=myTwitter',
+			'index.php?pf=myTwitter/icon-small.png',
+			'index.php?pf=myTwitter/icon-big.png',
+			'usage,contentadmin',
+			null,
+			null
+		));
+	}
+}
 ?>
